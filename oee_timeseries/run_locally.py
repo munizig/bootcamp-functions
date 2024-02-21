e440316c1d6802a959a7b0f7f4bcc3725e62fc42
Igor Muniz <39506738+munizig@users.noreply.github.com>
2024-02-21 16:05:04 +0000

    updating yaml files

--- oee_timeseries/run_locally.py
+++ oee_timeseries/run_locally.py
@@ -12,15 +12,12 @@ if __name__ == "__main__":
     data = {
         "sites": [
             "Oslo",
-            "Hannover",
             "Nuremberg",
             "Marseille",
             "Houston",
             "Sao Paulo",
             "Kuala Lumpur",
-            "Chicago",
-            "Rotterdam",
-            "London",
+            "Chicago"
         ],
         "data_set_external_id": "uc:001:oee:ds",
     }
